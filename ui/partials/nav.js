73d114a2208dee7b1481763fa8e96ddca99b051a
Robert Christian <rchristian@truecar.com>
2015-08-28 03:21:44 -0700

    Restructure; allow all requires from a root path: app/.

--- ui/partials/nav.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  'div.row':{
-    'button href="/todos" innerHTML="All"':0,
-    'button href="/todos-incomplete" innerHTML="Incomplete"':0,
-    'button href="/todos-complete" innerHTML="Completed"':0,
-  }
-}
